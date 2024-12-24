60c6f341ece9bd2a386792741f9ace0ec5fd41c5
Le Dieu <hongledt1905@gmail.com>
2024-12-25 01:28:30 +0700

    add business & data layer files

---
+++ starter/backend/src/businessLogic/todo.mjs
@@ -0,0 +1,31 @@
+import { v4 as uuidv4 } from 'uuid'
+import { TodoActions } from '../dataLayer/todoActions.mjs'
+
+const todoActions = new TodoActions()
+
+export async function getToDoList(userId) {
+  return await todoActions.getToDoList(userId)
+}
+
+export async function createTodo(createTodoRequest, userId) {
+  const todoId = uuidv4()
+  const createdAt = new Date().toISOString()
+
+  return await todoActions.createTodo({
+    todoId: todoId,
+    userId: userId,
+    attachmentUrl: '',
+    dueDate: createTodoRequest.dueDate,
+    createdAt: createdAt,
+    name: createTodoRequest.name,
+    done: false
+  })
+}
+
+export async function updateTodo(userId, todoId, updatedTodoRequest) {
+  return await todoActions.updateTodo(userId, todoId, updatedTodoRequest)
+}
+
+export async function deleteTodo(userId, todoId) {
+  return await todoActions.deleteTodo(userId, todoId)
+}
---
+++ starter/backend/src/dataLayer/todoActions.mjs
@@ -0,0 +1,76 @@
+import { DynamoDB } from '@aws-sdk/client-dynamodb'
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb'
+import AWSXRay from 'aws-xray-sdk-core'
+
+export class TodoActions {
+  constructor(
+    dynamoDb = AWSXRay.captureAWSv3Client(new DynamoDB()),
+    todosTable = process.env.TODOS_TABLE
+  ) {
+    this.dynamoDb = dynamoDb
+    this.todosTable = todosTable
+    this.dynamoDbClient = DynamoDBDocument.from(this.dynamoDb)
+  }
+
+  async getToDoList(userId) {
+    console.log('Getting all TODO items')
+    const result = await dynamoDbClient.query({
+      TableName: this.todosTable,
+      KeyConditionExpression: 'userId = :userId',
+      ExpressionAttributeValues: {
+        ':userId': userId
+      },
+      ScanIndexForward: false
+    })
+
+    return result.Items
+  }
+
+  async createTodo(newItem) {
+    console.log(`Creating a TODO item with id ${newItem.todoId}`)
+    await dynamoDbClient.put({
+      TableName: this.todosTable,
+      Item: newItem
+    })
+
+    return newItem
+  }
+
+  async updateTodo(userId, todoId, updatedTodo) {
+    console.log(`Updating a TODO item with id ${todoId}`)
+    await dynamoDbClient.update({
+      TableName: this.todosTable,
+      Key: {
+        userId: userId,
+        todoId: todoId
+      },
+      UpdateExpression:
+        'SET #name = :newName, #dueDate = :newDueDate, #done = :newStatus',
+      ExpressionAttributeNames: {
+        '#name': 'name',
+        '#dueDate': 'dueDate',
+        '#done': 'done'
+      },
+      ExpressionAttributeValues: {
+        ':newName': updatedTodo.name,
+        ':newDueDate': updatedTodo.dueDate,
+        ':newStatus': updatedTodo.done
+      }
+    })
+
+    return ''
+  }
+
+  async deleteTodo(userId, todoId) {
+    console.log(`Deleting a TODO item with id ${todoId}`)
+    await dynamoDbClient.delete({
+      TableName: this.todosTable,
+      Key: {
+        userId: userId,
+        todoId: todoId
+      }
+    })
+
+    return ''
+  }
+}
