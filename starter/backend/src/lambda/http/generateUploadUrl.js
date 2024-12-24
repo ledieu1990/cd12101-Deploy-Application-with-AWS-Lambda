47085e108b4b78c12e061192620460e4f7e4b521
Le Dieu <hongledt1905@gmail.com>
2024-12-24 08:30:35 +0700

    update code

--- starter/backend/src/lambda/http/generateUploadUrl.js
+++ starter/backend/src/lambda/http/generateUploadUrl.js
@@ -1,7 +1,99 @@
-export function handler(event) {
-  const todoId = event.pathParameters.todoId
+import { DynamoDB } from '@aws-sdk/client-dynamodb'
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb'
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+import AWSXRay from 'aws-xray-sdk-core'
+import { v4 as uuidv4 } from 'uuid'
+import middy from '@middy/core'
+import cors from '@middy/http-cors'
+import httpErrorHandler from '@middy/http-error-handler'
+import { getUserId } from '../utils.mjs'
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+const dynamoDb = AWSXRay.captureAWSv3Client(new DynamoDB())
+const dynamoDbClient = DynamoDBDocument.from(dynamoDb)
+const s3Client = new S3Client()
+
+const todosTable = process.env.TODOS_TABLE
+const imagesTable = process.env.IMAGES_TABLE
+const bucketName = process.env.IMAGES_S3_BUCKET
+const urlExpiration = parseInt(process.env.SIGNED_URL_EXPIRATION)
+
+export const handler = middy()
+  .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true
+    })
+  )
+  .handler(async (event) => {
+    console.log('Caller event', event)
+    const todoId = event.pathParameters.todoId
+    const userId = getUserId(event)
+    const validtodoId = await todoIdExists(todoId, userId)
+
+    if (!validtodoId) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          error: 'Todo item does not exist'
+        })
+      }
+    }
+
+    const imageId = uuidv4()
+    const newItem = await createImage(todoId, imageId, event)
+
+    const url = await getUploadUrl(imageId)
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        uploadUrl: url
+      })
+    }
+  })
+
+async function todoIdExists(todoId, userId) {
+  const result = await dynamoDbClient.get({
+    TableName: todosTable,
+    Key: {
+      userId: userId,
+      todoId: todoId
+    }
+  })
+
+  console.log('Get Todo item: ', result)
+  return !!result.Item
 }
 
+async function createImage(todoId, imageId, event) {
+  const timestamp = new Date().toISOString()
+  const newImage = JSON.parse(event.body)
+
+  const newItem = {
+    todoId,
+    createdAt,
+    imageId,
+    imageUrl: `https://${bucketName}.s3.amazonaws.com/${imageId}`,
+    ...newImage
+  }
+  console.log('Storing new item: ', newItem)
+
+  await dynamoDbClient.put({
+    TableName: imagesTable,
+    Item: newItem
+  })
+
+  return newItem
+}
+
+async function getUploadUrl(imageId) {
+  const command = new PutObjectCommand({
+    Bucket: bucketName,
+    Key: imageId
+  })
+  const url = await getSignedUrl(s3Client, command, {
+    expiresIn: urlExpiration
+  })
+  return url
+}
